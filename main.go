ce33ab9f8e9a90b91b7e8c0e88faffac3b7dcdc7
NaN <ipfilus@gmail.com>
2025-03-17 21:55:39 -0300

    feat: add temperatures on console with min, max and avg

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Measurement struct {
-	Name  string
 	Min   float64
 	Max   float64
 	Sum   float64
@@ -22,13 +23,37 @@ func main() {
 	}
 	defer measurements.Close()
 
+	data := make(map[string]Measurement)
+
 	scanner := bufio.NewScanner(measurements)
 	for scanner.Scan() {
 		rawData := scanner.Text()
 		semicolon := strings.Index(rawData, ";")
 		location := rawData[:semicolon]
-		temp := rawData[semicolon+1:]
-		fmt.Println(location, temp)
-		return
+		rawTemp := rawData[semicolon+1:]
+
+		temp, _ := strconv.ParseFloat(rawTemp, 64)
+
+		measurement, ok := data[location]
+		if !ok {
+			measurement = Measurement{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			measurement.Min = math.Min(measurement.Min, temp)
+			measurement.Max = math.Max(measurement.Max, temp)
+			measurement.Sum += temp
+			measurement.Count++
+		}
+
+		data[location] = measurement
+	}
+
+	for name, measurement := range data {
+		avg := measurement.Sum / float64(measurement.Count)
+		fmt.Printf("%s: min=%.1f, max=%.1f, avg=%.1f\n", name, measurement.Min, measurement.Max, avg)
 	}
 }
