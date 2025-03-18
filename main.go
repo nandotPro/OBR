b105b0b96517b090c99cec2f88c8bf83d71a8d45
NaN <ipfilus@gmail.com>
2025-03-17 21:41:19 -0300

    feat: add scanner for measuremets

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type Measurement struct {
+	Name  string
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int64
+}
+
 func main() {
-	fmt.Println("Bem-vindo ao meu projeto Go!")
+	measurements, err := os.Open("measurements.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer measurements.Close()
+
+	scanner := bufio.NewScanner(measurements)
+	for scanner.Scan() {
+		rawData := scanner.Text()
+		semicolon := strings.Index(rawData, ";")
+		location := rawData[:semicolon]
+		temp := rawData[semicolon+1:]
+		fmt.Println(location, temp)
+		return
+	}
 }
